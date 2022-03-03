5b69e79cfa5714f54ff987cc8eac567c71ac200d
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-03-03 03:22:49 +0000

    Thu, Mar 3, 2022, 3:22 AM +00:00

---
+++ mario-less/mario.c
@@ -0,0 +1,32 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // declare integer
+    int height;
+    // Prompt the user to get the height of the mario pyramid
+    do
+    {
+        height = get_int("Height: ");
+    } while (height < 1 || height > 8); // Prompt untill valid input
+    // Build rows in accordance with the height of the pyramid
+    for (int i = 0; i < height; i++)
+    {
+        // Print # for each row
+        // for (int j = height; j > 1; j--)
+        for (int j = 0; j <= i; ++j)
+        {
+            printf("#");
+            // if (j <= i)
+            // {
+            //     printf("#");
+            // }
+            // else
+            // {
+            //     printf("5");
+            // }
+        }
+        printf("#\n");
+    }
+}
