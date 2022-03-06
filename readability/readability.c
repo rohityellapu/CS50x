a7e67e70714e5154e99215dd1eaee4936209efaf
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-03-06 13:10:28 +0000

    Sun, Mar 6, 2022, 1:10 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -16,12 +16,7 @@ int main(void)
     float L = letters / words * 100;
     float S = sentences / words * 100;
     int index = 0.0588 * L - 0.296 * S - 15.8;
-    // printf("%f L\n", L);
-    // printf("%f S\n", S);
-    // printf("%f letters\n", letters);
-    // printf("%f words\n", words);
-    // printf("%f sentences\n", sentences);
-    // printf("%f index\n", index);
+
     if (index < 1)
     {
         printf("Before Grade 1\n");
