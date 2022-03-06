64237fba02165ed88e24041e4a7ed936ab391369
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-03-06 03:28:02 +0000

    Sun, Mar 6, 2022, 3:28 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,7 +1,26 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int count_letters(string text);
 
 int main(void)
 {
-    string text = get_string("Text: ");
+    string txt = get_string("Text: ");
+    int ltrs = count_letters(txt);
+    printf("%i\n", ltrs);
+}
+
+int count_letters(string text)
+{
+    int letters;
+    for (i = 0, n = strlen(text); i < n; i++)
+    {
+        if (!isspace(i))
+        {
+            letters++;
+        }
+    }
+    return letters;
 }
