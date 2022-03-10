498f52538ed0c178c2296e91b82b33e7455cbe78
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-03-10 17:12:02 +0000

    Thu, Mar 10, 2022, 5:12 PM +00:00

---
+++ caesar/caesar.c
@@ -0,0 +1,36 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+int check_key(string key);
+
+int main(int argc, string argv[])
+{
+    // printf("%i %s\n", argc, argv[1]);
+    if (argc == 2)
+    {
+        int k = check_key(argv[1]);
+        printf("%i\n", k);
+        return 0;
+    }
+    else
+    {
+        printf("Usage: %s key\n", argv[0]);
+        return 1;
+
+    }
+}
+
+int check_key(string key)
+{
+    for (int i = 0; i < strlen(key); i++)
+    {
+        if (!isdigit(key[i]))
+        {
+            return 1;
+        }
+    }
+    return atoi(key);
+}
