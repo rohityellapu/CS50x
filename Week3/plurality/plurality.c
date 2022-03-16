e4ecef606f131c01b99e7014b7fc194b7fa7df69
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-03-16 16:52:42 +0000

    Wed, Mar 16, 2022, 4:52 PM +00:00

--- Week3/plurality/plurality.c
+++ Week3/plurality/plurality.c
@@ -66,7 +66,7 @@ int main(int argc, string argv[])
 bool vote(string name)
 {
     // TODO
-    for (int i = 0; i <= candidate_count; i++)
+    for (int i = 0; i < candidate_count; i++)
     {
         if (strcmp(candidates[i].name, name) ==0)
         {
