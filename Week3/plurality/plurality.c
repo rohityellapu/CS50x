f70400ea08bcccdde5e02912ddfa0fe62612a93b
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-03-22 13:43:28 +0000

    Tue, Mar 22, 2022, 1:43 PM +00:00

--- Week3/plurality/plurality.c
+++ Week3/plurality/plurality.c
@@ -81,7 +81,7 @@ bool vote(string name)
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-    string winner;
+    string winner[];
     // TODO
     for (int i = 1; i < candidate_count; i++)
     {
