39d878d992ff25b7f612efc5696a4d52340f461c
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-03-27 06:22:57 +0000

    Sun, Mar 27, 2022, 6:22 AM +00:00

--- Week3/runoff/runoff.c
+++ Week3/runoff/runoff.c
@@ -133,7 +133,7 @@ bool vote(int voter, int rank, string name)
     {
         if (strcmp(name, candidates[i].name) ==0)
         {
-            
+            preferences
         }
     }
         return false;
