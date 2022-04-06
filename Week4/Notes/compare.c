570011bf4ba9df7eacba00f88dbbacc746570805
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-04-06 13:17:45 +0000

    Wed, Apr 6, 2022, 1:17 PM +00:00

--- Week4/Notes/compare.c
+++ Week4/Notes/compare.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <string.h>
 
 int main(void)
 {
@@ -15,15 +16,18 @@ int main(void)
     // {
     //     printf("Different\n");
     // }
-    int s = get_int("i: ");
-    int q = get_int("j: ");
+    char *s = get_string("i: ");
+    char *q = get_string("j: ");
 
-    if (s == q)
-    {
-        printf("Same\n");
-    }
-    else
-    {
-        printf("Different\n");
-    }
+    // if (strcmp(s, q) == 0) deleted by if(s==q) which doesn't work
+    // {
+    //     printf("Same\n");
+    // }
+    // else
+    // {
+    //     printf("Different\n");
+    // }
+    printf("%p\n", s);
+    printf("%p\n", q);
+
 }
