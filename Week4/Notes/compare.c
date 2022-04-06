c967ea2afcbdcc6f12f73b3ab202966a679bc382
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-04-06 13:07:07 +0000

    Wed, Apr 6, 2022, 1:07 PM +00:00

--- Week4/Notes/compare.c
+++ Week4/Notes/compare.c
@@ -3,6 +3,18 @@
 
 int main(void)
 {
+    // Integers Comparison
+    // int s = get_int("i: ");
+    // int q = get_int("j: ");
+
+    // if (s == q)
+    // {
+    //     printf("Same\n");
+    // }
+    // else
+    // {
+    //     printf("Different\n");
+    // }
     int s = get_int("i: ");
     int q = get_int("j: ");
 
