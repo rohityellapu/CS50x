82f93e5404bd8b1a03305f3dce70830493decd14
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-04-06 16:17:26 +0000

    Wed, Apr 6, 2022, 4:17 PM +00:00

--- Week4/Notes/copy.c
+++ Week4/Notes/copy.c
@@ -7,7 +7,8 @@ int main(void)
 {
     string s = get_string("s: ");
 
-    string t = s;
+    // string t = s; This means t just copied the address of s (changes on t means changes on s)
+    
     t[0] = toupper(t[0]);
 
     printf("s: %s\n", s);
