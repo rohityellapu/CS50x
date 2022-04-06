02d0e543505571bb9b5693e676df681310375796
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-04-06 16:22:38 +0000

    Wed, Apr 6, 2022, 4:22 PM +00:00

--- Week4/Notes/copy.c
+++ Week4/Notes/copy.c
@@ -5,12 +5,25 @@
 
 int main(void)
 {
-    string s = get_string("s: ");
+    char *s = get_string("s: ");
 
     // string t = s; This means t just copied the address of s (changes on t means changes on s)
-    
-    t[0] = toupper(t[0]);
+
+    char *t = malloc(strlen(s) + 1);
+    if (t == NULL)
+    {
+        return 1;
+    }
+    strcopy(t, s);
+
+    if (strlen(t) > 1)
+    {
+        t[0] = toupper(t[0]);
+    }
+
 
     printf("s: %s\n", s);
     printf("t: %s\n", t);
+
+    free(t);
 }
