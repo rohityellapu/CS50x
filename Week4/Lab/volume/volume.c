799e6082ca2903abe8c0812bb671f6b25f1bdfea
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-05-26 02:28:35 +0000

    Thu, May 26, 2022, 2:28 AM +00:00

--- Week4/Lab/volume/volume.c
+++ Week4/Lab/volume/volume.c
@@ -35,9 +35,11 @@ int main(int argc, char *argv[])
 
     // TODO: Copy header from input file to output file
 
-    uint8_t header[n];
+    uint8_t header[HEADER_SIZE];
 
     
+
+
 
     // TODO: Read samples from input file and write updated data to output file
 
