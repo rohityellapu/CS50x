bea9285ff5fc926bb43320c32f47c37c200b96f3
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-06-05 09:52:01 +0000

    Sun, Jun 5, 2022, 9:52 AM +00:00

--- Week4/PSETS/Filter/filter-less/helpers.c
+++ Week4/PSETS/Filter/filter-less/helpers.c
@@ -3,9 +3,9 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    for (int i = 0, i < height, i++)
+    for (int i = 0; i < height; i++)
     {
-        for (int j = 0, j < width, i++)
+        for (int j = 0; j < width; j++)
         {
             RGBTRIPLE *pixel = &image[i][j];
             int avg = round(((*pixel).rgbtBlue + (*pixel).rgbtGreen + (*pixel).rgbtRed) / 3.0);
