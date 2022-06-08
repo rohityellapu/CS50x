c17abaa7dc39144fb295ae43349d74be850aa2ca
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-06-08 10:27:01 +0000

    Wed, Jun 8, 2022, 10:27 AM +00:00

--- Week4/PSETS/Filter/filter-less/helpers.c
+++ Week4/PSETS/Filter/filter-less/helpers.c
@@ -1,7 +1,6 @@
 #include <math.h>
 #include "helpers.h"
 
-
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
@@ -27,13 +26,27 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            RGBTRIPLE *pixel = &image[i][j];
+            // RGBTRIPLE *pixel = &image[i][j];
+            image[i][j].rgbtBlue = round(0.272 * image[i][j].rgbtRed + 0.534 * image[i][j].rgbtGreen + 0.131 * image[i][j].rgbtBlue);
+            image[i][j].rgbtGreen = round(0.349 * image[i][j].rgbtRed + 0.686 * image[i][j].rgbtGreen + 0.168 * image[i][j].rgbtBlue);
+            image[i][j].rgbtRed = round(0.393 * image[i][j].rgbtRed + 0.769 * image[i][j].rgbtGreen + 0.189 * image[i][j].rgbtBlue);
 
-            
+            if (image[i][j].rgbtBlue > 255)
+            {
+                image[i][j].rgbtBlue = 255;
+            }
+            if (image[i][j].rgbtGreem > 255)
+            {
+                image[i][j].rgbtGreem = 255;
+            }
+            if (image[i][j].rgbtRed > 255)
+            {
+                image[i][j].rgbtRed = 255;
+            }
         }
     }
 
-        return;
+    return;
 }
 
 // Reflect image horizontally
