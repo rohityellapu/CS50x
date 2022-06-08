2cc1ccf577f60642eb47846ccf04873d62ea00fd
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-06-08 10:37:37 +0000

    Wed, Jun 8, 2022, 10:37 AM +00:00

--- Week4/PSETS/Filter/filter-less/helpers.c
+++ Week4/PSETS/Filter/filter-less/helpers.c
@@ -48,22 +48,22 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             // (*pixel).rgbtGreen = sepiaGreen;
             // (*pixel).rgbtRed = sepiaRed;
             // // RGBTRIPLE *pixel = &image[i][j];
-            // image[i][j].rgbtBlue = round(0.272 * image[i][j].rgbtRed + 0.534 * image[i][j].rgbtGreen + 0.131 * image[i][j].rgbtBlue);
-            // image[i][j].rgbtGreen = round(0.349 * image[i][j].rgbtRed + 0.686 * image[i][j].rgbtGreen + 0.168 * image[i][j].rgbtBlue);
-            // image[i][j].rgbtRed = round(0.393 * image[i][j].rgbtRed + 0.769 * image[i][j].rgbtGreen + 0.189 * image[i][j].rgbtBlue);
+            image[i][j].rgbtBlue = round(0.272 * image[i][j].rgbtRed + 0.534 * image[i][j].rgbtGreen + 0.131 * image[i][j].rgbtBlue);
+            image[i][j].rgbtGreen = round(0.349 * image[i][j].rgbtRed + 0.686 * image[i][j].rgbtGreen + 0.168 * image[i][j].rgbtBlue);
+            image[i][j].rgbtRed = round(0.393 * image[i][j].rgbtRed + 0.769 * image[i][j].rgbtGreen + 0.189 * image[i][j].rgbtBlue);
 
-            // if (image[i][j].rgbtBlue > 255)
-            // {
-            //     image[i][j].rgbtBlue = 255;
-            // }
-            // if (image[i][j].rgbtGreen > 255)
-            // {
-            //     image[i][j].rgbtGreen = 255;
-            // }
-            // if (image[i][j].rgbtRed > 255)
-            // {
-            //     image[i][j].rgbtRed = 255;
-            // }
+            if (image[i][j].rgbtBlue > 255)
+            {
+                image[i][j].rgbtBlue = 255;
+            }
+            if (image[i][j].rgbtGreen > 255)
+            {
+                image[i][j].rgbtGreen = 255;
+            }
+            if (image[i][j].rgbtRed > 255)
+            {
+                image[i][j].rgbtRed = 255;
+            }
         }
     }
 
