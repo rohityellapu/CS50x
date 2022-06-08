b2368457d7b9a2408ff8d6aaf074ebe61852c49b
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-06-08 10:47:16 +0000

    Wed, Jun 8, 2022, 10:47 AM +00:00

--- Week4/PSETS/Filter/filter-less/helpers.c
+++ Week4/PSETS/Filter/filter-less/helpers.c
@@ -1,14 +1,14 @@
 #include <math.h>
 #include "helpers.h"
 
-// Convert (*pixel)ayscale
-void grayscale(int height, int width, RGBTRIPLE (*pixel)t][width])
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            RGBTRIPLE *pixel = &(*pixel);
+            RGBTRIPLE *pixel = &image[i][j];
             int avg = round(((*pixel).rgbtBlue + (*pixel).rgbtGreen + (*pixel).rgbtRed) / 3.0);
 
             (*pixel).rgbtBlue = avg;
@@ -19,14 +19,14 @@ void grayscale(int height, int width, RGBTRIPLE (*pixel)t][width])
     return;
 }
 
-// Convert (*pixel)pia
-void sepia(int height, int width, RGBTRIPLE (*pixel)t][width])
+// Convert image to sepia
+void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            // RGBTRIPLE *pixel = &(*pixel);
+            // RGBTRIPLE *pixel = &image[i][j];
             // int sepiaRed = round(0.189 * (*pixel).rgbtBlue + 0.769 * (*pixel).rgbtGreen + 0.393 * (*pixel).rgbtRed);
             // int sepiaGreen = round(0.168 * (*pixel).rgbtBlue + 0.686 * (*pixel).rgbtGreen + 0.349 * (*pixel).rgbtRed);
             // int sepiaBlue = round(0.131 * (*pixel).rgbtBlue + 0.534 * (*pixel).rgbtGreen + 0.272 * (*pixel).rgbtRed);
@@ -47,22 +47,22 @@ void sepia(int height, int width, RGBTRIPLE (*pixel)t][width])
             // (*pixel).rgbtBlue = sepiaBlue;
             // (*pixel).rgbtGreen = sepiaGreen;
             // (*pixel).rgbtRed = sepiaRed;
-            RGBTRIPLE *pixel = &image[i][j];
-            (*pixel).rgbtBlue = round(0.272 * (*pixel).rgbtRed + 0.534 * (*pixel).rgbtGreen + 0.131 * (*pixel).rgbtBlue);
-            (*pixel).rgbtGreen = round(0.349 * (*pixel).rgbtRed + 0.686 * (*pixel).rgbtGreen + 0.168 * (*pixel).rgbtBlue);
-            (*pixel).rgbtRed = round(0.393 * (*pixel).rgbtRed + 0.769 * (*pixel).rgbtGreen + 0.189 * (*pixel).rgbtBlue);
+            // // RGBTRIPLE *pixel = &image[i][j];
+            image[i][j].rgbtBlue = round(0.272 * image[i][j].rgbtRed + 0.534 * image[i][j].rgbtGreen + 0.131 * image[i][j].rgbtBlue);
+            image[i][j].rgbtGreen = round(0.349 * image[i][j].rgbtRed + 0.686 * image[i][j].rgbtGreen + 0.168 * image[i][j].rgbtBlue);
+            image[i][j].rgbtRed = round(0.393 * image[i][j].rgbtRed + 0.769 * image[i][j].rgbtGreen + 0.189 * image[i][j].rgbtBlue);
 
-            if ((*pixel).rgbtBlue > 255)
+            if (image[i][j].rgbtBlue > 255)
             {
-                (*pixel).rgbtBlue = 255;
+                image[i][j].rgbtBlue = 255;
             }
-            if ((*pixel).rgbtGreen > 255)
+            if (image[i][j].rgbtGreen > 255)
             {
-                (*pixel).rgbtGreen = 255;
+                image[i][j].rgbtGreen = 255;
             }
-            if ((*pixel).rgbtRed > 255)
+            if (image[i][j].rgbtRed > 255)
             {
-                (*pixel).rgbtRed = 255;
+                image[i][j].rgbtRed = 255;
             }
         }
     }
@@ -70,13 +70,14 @@ void sepia(int height, int width, RGBTRIPLE (*pixel)t][width])
     return;
 }
 
-// Reflect (*pixel)ontally
-void reflect(int height, int width, RGBTRIPLE (*pixel)t][width])
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
     return;
 }
 
-// Blur (*pixel)blur(int height, int width, RGBTRIPLE (*pixel)t][width])
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
 {
     return;
 }
