45119dea0f7750b04ea20d79769cbf55a279149a
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-06-08 10:17:57 +0000

    Wed, Jun 8, 2022, 10:17 AM +00:00

--- Week4/PSETS/Filter/filter-less/helpers.c
+++ Week4/PSETS/Filter/filter-less/helpers.c
@@ -23,9 +23,17 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
-    
+    for (int i = 0; i < heigt; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            RGBTRIPLE *pixel = &image[i][j];
 
-    return;
+            
+        }
+    }
+
+        return;
 }
 
 // Reflect image horizontally
