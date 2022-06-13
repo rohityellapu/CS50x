f8ff8e826a3360af28f091c15463a43e40f276a9
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-06-13 08:38:30 +0000

    Mon, Jun 13, 2022, 8:38 AM +00:00

--- Week4/PSETS/Filter/filter-less/bmp.h
+++ Week4/PSETS/Filter/filter-less/bmp.h
@@ -55,3 +55,97 @@ typedef struct
     BYTE  rgbtRed;
 } __attribute__((__packed__))
 RGBTRIPLE;
+
+
+ // // Make a copy of the image
+    RGBTRIPLE pixel[height][width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            pixel[i][j] = image[i][j];
+        }
+    }
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int count = 0, red = 0, green = 0, blue = 0;
+            // for current pixel.
+            red += pixel[i][j].rgbtRed;
+            green += pixel[i][j].rgbtGreen;
+            blue += pixel[i][j].rgbtBlue;
+            count++;
+            // Check if pixel exists directly above the current pixel
+            if (i - 1 >= 0 && j >= 0)
+            {
+                red += pixel[i - 1][j].rgbtRed;
+                green += pixel[i - 1][j].rgbtGreen;
+                blue += pixel[i - 1][j].rgbtBlue;
+                count++;
+            }
+            // Check if pixel exists directly below the current pixel
+            if (i + 1 < height && j >= 0)
+            {
+                red += pixel[i + 1][j].rgbtRed;
+                green += pixel[i + 1][j].rgbtGreen;
+                blue += pixel[i + 1][j].rgbtBlue;
+                count++;
+            }
+            // Check if pixel exists directly left the current pixel
+            if (j - 1 >= 0)
+            {
+                red += pixel[i][j - 1].rgbtRed;
+                green += pixel[i][j - 1].rgbtGreen;
+                blue += pixel[i][j - 1].rgbtBlue;
+                count++;
+            }
+            // Check if pixel exists directly right the current pixel
+            if (j + 1 < width)
+            {
+                red += pixel[i][j + 1].rgbtRed;
+                green += pixel[i][j + 1].rgbtGreen;
+                blue += pixel[i][j + 1].rgbtBlue;
+                count++;
+            }
+            // Check if pixel exists diagnally left above the current pixel
+
+            if (i - 1 >= 0 && j - 1 >= 0)
+            {
+                red += pixel[i - 1][j - 1].rgbtRed;
+                green += pixel[i - 1][j - 1].rgbtGreen;
+                blue += pixel[i - 1][j - 1].rgbtBlue;
+                count++;
+            }
+            // Check if pixel exists diagnally right above the current pixel
+            if (i - 1 >= 0 && j + 1 < width)
+            {
+                red += pixel[i - 1][j + 1].rgbtRed;
+                green += pixel[i - 1][j + 1].rgbtGreen;
+                blue += pixel[i - 1][j + 1].rgbtBlue;
+                count++;
+            }
+
+            // Check if pixel exists diagnally left below the current pixel
+            if (j - 1 >= 0 && i + 1 < height)
+            {
+                red += pixel[i + 1][j - 1].rgbtRed;
+                green += pixel[i + 1][j - 1].rgbtGreen;
+                blue += pixel[i + 1][j - 1].rgbtBlue;
+                count++;
+            }
+            // Check if pixel exists diagnally right below the current pixel
+            if (j + 1 < width && i + 1 < height)
+            {
+                red += pixel[i + 1][j + 1].rgbtRed;
+                green += pixel[i + 1][j + 1].rgbtGreen;
+                blue += pixel[i + 1][j + 1].rgbtBlue;
+                count++;
+            }
+
+            // Average the pixel with surrounded neighbhor pixels
+            image[i][j].rgbtRed = round(red / count * 1.0);
+            image[i][j].rgbtGreen = round(green / count * 1.0);
+            image[i][j].rgbtBlue = round(blue / count * 1.0);
+        }
