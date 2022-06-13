d8b23f54b5ef416a38ac73233f5a738b8582c93a
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-06-13 07:57:23 +0000

    Mon, Jun 13, 2022, 7:57 AM +00:00

--- Week4/PSETS/Filter/filter-less/helpers.c
+++ Week4/PSETS/Filter/filter-less/helpers.c
@@ -26,28 +26,7 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            // RGBTRIPLE *pixel = &image[i][j];
-            // int sepiaRed = round(0.189 * (*pixel).rgbtBlue + 0.769 * (*pixel).rgbtGreen + 0.393 * (*pixel).rgbtRed);
-            // int sepiaGreen = round(0.168 * (*pixel).rgbtBlue + 0.686 * (*pixel).rgbtGreen + 0.349 * (*pixel).rgbtRed);
-            // int sepiaBlue = round(0.131 * (*pixel).rgbtBlue + 0.534 * (*pixel).rgbtGreen + 0.272 * (*pixel).rgbtRed);
-
-            // if (sepiaRed > 255)
-            // {
-            //     sepiaRed = 255;
-            // }
-            // if (sepiaGreen > 255)
-            // {
-            //     sepiaGreen = 255;
-            // }
-            // if (sepiaBlue > 255)
-            // {
-            //     sepiaBlue = 255;
-            // }
-
-            // (*pixel).rgbtBlue = sepiaBlue;
-            // (*pixel).rgbtGreen = sepiaGreen;
-            // (*pixel).rgbtRed = sepiaRed;
-            // // RGBTRIPLE *pixel = &image[i][j];
+            
             int sepiaBlue = round(0.272 * image[i][j].rgbtRed + 0.534 * image[i][j].rgbtGreen + 0.131 * image[i][j].rgbtBlue);
             int sepiaGreen = round(0.349 * image[i][j].rgbtRed + 0.686 * image[i][j].rgbtGreen + 0.168 * image[i][j].rgbtBlue);
             int sepiaRed = round(0.393 * image[i][j].rgbtRed + 0.769 * image[i][j].rgbtGreen + 0.189 * image[i][j].rgbtBlue);
