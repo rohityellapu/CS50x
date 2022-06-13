49566ed863736885bbf891a5bf309275bbd33184
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-06-13 09:15:20 +0000

    Mon, Jun 13, 2022, 9:15 AM +00:00

--- Week4/PSETS/Filter/filter-less/helpers.c
+++ Week4/PSETS/Filter/filter-less/helpers.c
@@ -82,7 +82,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
     RGBTRIPLE pixel[height][width];
-     for (int i = 0; i < height; i++)
+    for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
