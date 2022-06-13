383756ddc85c7795a4c274c72d4db485aa2cc294
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-06-13 16:07:55 +0000

    Mon, Jun 13, 2022, 4:07 PM +00:00

--- Week4/PSETS/Recover/recover/recover.c
+++ Week4/PSETS/Recover/recover/recover.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main(int argc, char *argv[])
 {
- 
+    if (argc != 2)
+    {
+        printf("Usage: ./recover <forensic_image>\n");
+        return 1;
+    }
+
+    char *image = argv[1];
+
+    FILE *input = fopen(image, "r");
+
+    if (input == NULL)
+    {
+        printf("Could not open %s.\n", image);
+        return 2;
+    }
+    
+
+
+
 }
