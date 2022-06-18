1558537674eddca7258e4e36b9ed40893b1e9e21
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-06-18 05:07:04 +0000

    Sat, Jun 18, 2022, 5:07 AM +00:00

--- Week4/PSETS/Recover/recover/recover.c
+++ Week4/PSETS/Recover/recover/recover.c
@@ -2,37 +2,48 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+// define new BYTE type
 typedef uint8_t BYTE;
+
 #define BLOCK 512
 
 int main(int argc, char *argv[])
 {
     if (argc != 2)
     {
+        // Check for the correct input usage
         printf("Usage: ./recover <forensic_image>\n");
         return 1;
     }
 
+    // Open input file
     FILE *input = fopen(argv[1], "r");
 
+    // Check if the input is not null
     if (input == NULL)
     {
         printf("Could not open %s.\n", argv[1]);
         return 2;
     }
 
+    // declare name of the image
     char recovered_image[8];
 
+    // allocate storage for buffer in the memory
     BYTE *buffer = malloc(BLOCK * sizeof(BYTE));
+
+    // End program if there isn't enough memory
     if (buffer == NULL)
     {
         printf("No enough memort to store image.\n");
         return 3;
     }
 
+    // Point the image address to NULL
     FILE *jpg = NULL;
     int jpg_count = 0;
 
+    
     while (fread(buffer, sizeof(buffer), 1, input) == 1)
     {
 
