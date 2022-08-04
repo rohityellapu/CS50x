40b01a93289b79ae7a65db06ebf46988990193fb
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-08-04 10:01:34 +0000

    Thu, Aug 4, 2022, 10:01 AM +00:00

--- Week5/Lab/inheritance/inheritance.c
+++ Week5/Lab/inheritance/inheritance.c
@@ -10,8 +10,7 @@ typedef struct person
 {
     struct person *parents[2];
     char alleles[2];
-}
-person;
+} person;
 
 const int GENERATIONS = 10;
 const int INDENT_LENGTH = 4;
@@ -46,34 +45,33 @@ person *create_family(int generations)
         printf("No enough Memory\n");
         return NULL;
     }
-        // If there are still generations left to create
-        if (generations > 1)
-        {
-            // Create two new parents for current person by recursively calling create_family
-            person *parent0 = create_family(generations - 1);
-            person *parent1 = create_family(generations - 1);
+    // If there are still generations left to create
+    if (generations > 1)
+    {
+        // Create two new parents for current person by recursively calling create_family
+        person *parent0 = create_family(generations - 1);
+        person *parent1 = create_family(generations - 1);
 
-            // TODO: Set parent pointers for current person
-            n->parents[0] = parent0;
-            n->parents[1] = parent1;
+        // TODO: Set parent pointers for current person
+        n->parents[0] = parent0;
+        n->parents[1] = parent1;
 
-            // TODO: Randomly assign current person's alleles based on the alleles of their parents
-            n->alleles[0] = parent0->alleles[rand() % 2];
-            n->alleles[1] = parent1->alleles[rand() % 2];
+        // TODO: Randomly assign current person's alleles based on the alleles of their parents
+        n->alleles[0] = parent0->alleles[rand() % 2];
+        n->alleles[1] = parent1->alleles[rand() % 2];
+    }
 
-        }
+    // If there are no generations left to create
+    else
+    {
+        // TODO: Set parent pointers to NULL
+        n->parents[0] = NULL;
+        n->parents[1] = NULL;
 
-        // If there are no generations left to create
-        else
-        {
-            // TODO: Set parent pointers to NULL
-            n->parents[0] = NULL;
-            n->parents[1] = NULL;
-
-            // TODO: Randomly assign alleles
-            n->alleles[0] = random_allele();
-            n->alleles[1] = random_allele();
-        }
+        // TODO: Randomly assign alleles
+        n->alleles[0] = random_allele();
+        n->alleles[1] = random_allele();
+    }
 
     // TODO: Return newly created person
     return n;
