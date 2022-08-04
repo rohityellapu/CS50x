bb9bcfe0d2ebfcf7e7af7cd16fe879b1b23d3cd5
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-08-04 10:20:06 +0000

    Thu, Aug 4, 2022, 10:20 AM +00:00

--- Week5/Notes/node.c
+++ Week5/Notes/node.c
@@ -20,4 +20,41 @@ int main(void)
     n->next = NULL;
 
     list = n;
+
+    n = malloc(sizeof(node));
+    if (n == NULL)
+    {
+        free(list);
+        return 1;
+    }
+    n->number = 2;
+    n->next = NULL;
+
+    list->next = n;
+
+    n = malloc(sizeof(node));
+    if (n == NULL)
+    {
+        free(list->next);
+        free(list);
+        return 1;
+    }
+
+    n->number = 3;
+    n->next = NULL;
+
+    list->next->next = n;
+
+    for (node *tmp = list; tmp != NULL; tmp = tmp->next)
+    {
+        printf('Number is, %i\n', tmp->number);
+    }
+
+    while (tmp != NULL)
+    {
+        node *tmp = list - next;
+        free(list);
+        list = tmp;
+    }
+    return 0;
 }
