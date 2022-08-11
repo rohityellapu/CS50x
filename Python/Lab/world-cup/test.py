d1fa5ae107a076ef3e690d0245da0e9c3034f896
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-08-11 11:22:39 +0000

    ddd, Aug 8/11/2022, YYYY, 11:22 A +0

--- Python/Lab/world-cup/test.py
+++ Python/Lab/world-cup/test.py
@@ -4,4 +4,11 @@ import sys
 def main():
     if len(sys.argv) != 2:
         sys.exit("Usage: pythonfile.py filename\n")
-    file = 
+    f = open(sys.argv[1])
+    data = csv.DictReader(f)
+    print(data)
+
+
+
+
+main()
