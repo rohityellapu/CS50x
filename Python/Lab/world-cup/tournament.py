4f92fd1eec0ee09f42fde6c134d9be0c9f85f935
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-08-13 08:05:18 +0000

    Sat, Aug 13, 2022, 8:05 AM +00:00

--- Python/Lab/world-cup/tournament.py
+++ Python/Lab/world-cup/tournament.py
@@ -26,6 +26,12 @@ def main():
             teams.append(row)
             counts[row['team']] = 0
 
+    for i in range(N):
+        winner = simulate_tournament(teams)
+        if winner in counts:
+            counts[winner] += 1
+        else:
+            counts[winner] = 1
     # Print each team's chances of winning, according to simulation
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
         print(f"{team}: {counts[team] * 100 / N:.1f}% chance of winning")
@@ -56,6 +62,9 @@ def simulate_round(teams):
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
     # TODO
+    while(len(teams) > 1):
+        teams = simulate_round(teams)
+    return teams[0]['team']
 
 
 if __name__ == "__main__":
