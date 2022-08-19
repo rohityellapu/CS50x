d99212b72ec4858e535147a0bda006d0ee49a465
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-08-19 07:16:44 +0000

    Fri, Aug 19, 2022, 7:16 AM +00:00

--- Python/PSETS/sentimental-mario-less/mario.py
+++ Python/PSETS/sentimental-mario-less/mario.py
@@ -1,10 +1,14 @@
 # TODO
 from cs50 import get_int
 
+# Get the height of the mario pyramid from user
 height = get_int("Height: ")
+
+# Prompt the user forever until valid height number
 while height < 1 or height > 8:
     height = get_int("Height: ")
 
+# For each row in the pyramid
 for i in range(height):
     for j in range(height-1, -1, -1):
         if j > i:
