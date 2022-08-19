333acbab43949f0a48ec150343ab03bd1e3b750e
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-08-19 08:47:20 +0000

    Fri, Aug 19, 2022, 8:47 AM +00:00

--- Week2/scrabble/scrabble.c
+++ Week2/scrabble/scrabble.c
@@ -24,8 +24,9 @@ int main(void)
 int compute_score(string word)
 {
     // TODO: Compute and return score for string
+    int score = 0;
     for (int i = 0; i < strlen(word);i++)
     {
-        
+        if(isLower(word[i]))
     }
 }
