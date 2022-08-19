ec9596d5e428a66223d6622257c0a4a606b633db
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-08-19 08:21:48 +0000

    Fri, Aug 19, 2022, 8:21 AM +00:00

--- Python/PSETS/sentimental-cash/cash.py
+++ Python/PSETS/sentimental-cash/cash.py
@@ -9,47 +9,55 @@ def main():
         change = get_float("Change owed: ")
 
     cents = change*100
-    print(cents%25)
+
     quarters = calculate_quarters(700)
     cents -= quarters*25
-    print(quarters)
+
     dimes = calculate_dimes(cents)
     cents -= dimes*10
-    print(dimes)
+
     nickels = calculate_nickels(cents)
     cents -= nickels*5
-    print(nickels)
+
     pennies = calculate_pennies(cents)
     cents -= pennies*1
-    print(cents)
+
     total_coins = quarters + dimes + nickels + pennies
     print(total_coins)
 
 def calculate_quarters(cents):
     quarter = 25
     if cents >= quarter:
-        return cents % quarter
+        remainder = cents % quarter
+        coins = (cents - remainder)/quarter
+        return coins
     else:
         return 0
 
 def calculate_dimes(cents):
     dime = 10
     if cents >= dime:
-        return cents%dime
+        remainder = cents % dime
+        coins = (cents - remainder)/dime
+        return coins
     else:
         return 0
 
 def calculate_nickels(cents):
     nickel = 5
     if cents >= nickel:
-        return cents%nickel
+        remainder = cents % nickel
+        coins = (cents - remainder)/nickel
+        return coins
     else:
         return 0
 
 def calculate_pennies(cents):
     penny = 1
     if cents >= penny:
-        return cents%penny
+        remainder = cents % penny
+        coins = (cents - remainder)/penny
+        return coins
     else:
         return 0
 
