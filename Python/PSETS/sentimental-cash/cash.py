c6a746244d6f003b8ae22c255477d716f152fa63
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-08-19 07:57:36 +0000

    Fri, Aug 19, 2022, 7:57 AM +00:00

--- Python/PSETS/sentimental-cash/cash.py
+++ Python/PSETS/sentimental-cash/cash.py
@@ -3,5 +3,25 @@
 from cs50 import get_float
 
 change = get_float("Change owed: ")
-total_cents = change*100
-quart
+
+total_cents = change * 100
+
+quarter = 25
+dime = 10
+nickel = 5
+penny = 1
+
+total_coins = 0
+
+if total_cents >= quarter:
+    total_coins += total_cents % quarter
+    total_cents %= quarter
+if total_cents >= quarter:
+    total_coins += total_cents % quarter
+    total_cents %= quarter
+if total_cents >= quarter:
+    total_coins += total_cents % quarter
+    total_cents %= quarter
+if total_cents >= quarter:
+    total_coins += total_cents % quarter
+    total_cents %= quarter
