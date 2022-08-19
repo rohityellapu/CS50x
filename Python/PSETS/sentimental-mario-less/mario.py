8cb38d40c73293487684f09f7413da4b3f6d4298
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-08-19 07:11:59 +0000

    Fri, Aug 19, 2022, 7:11 AM +00:00

--- Python/PSETS/sentimental-mario-less/mario.py
+++ Python/PSETS/sentimental-mario-less/mario.py
@@ -6,9 +6,8 @@ while height < 1 or height > 8:
     height = get_int("Height: ")
 
 for i in range(height):
-    for j in range(height-1, 0, -1):
-        print(j)
-        if j >= i:
+    for j in range(height-1, -1, -1):
+        if j > i:
             print(" ", end= "")
         else:
             print("#", end="")
