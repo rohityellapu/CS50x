03dc5551546f6b543ff35b3697051f19752883e2
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-08-19 16:16:49 +0000

    Fri, Aug 19, 2022, 4:16 PM +00:00

--- 1.C/credit/credit.c
+++ 1.C/credit/credit.c
@@ -3,15 +3,36 @@
 
 int main(void)
 {
-    int card_umber = get_long("Number: \n");
+    int card_number = get_long("Number: \n");
     while (card_number < 0)
     {
         card_number = get_long("Number: \n");
     }
 
+    int card = check_card_type(card_number);
 }
 
-int check_card_type(number)
+int check_card_type(double number)
 {
-    int first
+    int first_digit;
+    for (let i = 10; number > 0; number /= 10)
+    {
+        first_digit = number % i;
+    }
+    if(first_digit == 3)
+    {
+        return 1;
+    }
+    else if(first_digit == 5)
+    {
+        return 2;
+    }
+    else if(first_digit == 4)
+    {
+        return 3;
+    }
+    else
+    {
+        return 0;
+    }
 }
