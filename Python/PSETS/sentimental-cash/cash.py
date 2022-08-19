029344084997ef797edb08554ef068a8ef804d8f
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-08-19 08:08:45 +0000

    Fri, Aug 19, 2022, 8:08 AM +00:00

--- Python/PSETS/sentimental-cash/cash.py
+++ Python/PSETS/sentimental-cash/cash.py
@@ -10,25 +10,48 @@ def main():
 
     cents = change * 100
 
+    quarters = calculate_quarters(cents)
+    cents -= quarters*25
+
+    dimes = calculate_dimes(cents)
+    cents -= dimes*10
+
+    nickels = calculate_nickels(cents)
+    cents -= nickels*5
+
+    pennies = calculate_pennies(cents)
+    cents -= pennies*1
+
+    total_coins = quarters + dimes + nickels + pennies
+    print(total_coins)
 
 def calculate_quarters(cents):
     quarter = 25
-    
-dime = 10
-nickel = 5
-penny = 1
+    if cents >= quarter:
+        return cents % quarter
+    else:
+        return 0
 
-total_coins = 0
+def calculate_dimes(cents):
+    dime = 10
+    if cents >= dime:
+        return cents % dime
+    else:
+        return 0
 
-if total_cents >= quarter:
-    total_coins += total_cents % quarter
-    total_cents %= quarter
-if total_cents >= quarter:
-    total_coins += total_cents % quarter
-    total_cents %= quarter
-if total_cents >= quarter:
-    total_coins += total_cents % quarter
-    total_cents %= quarter
-if total_cents >= quarter:
-    total_coins += total_cents % quarter
-    total_cents %= quarter
+def calculate_nickels(cents):
+    nickel = 5
+    if cents >= nickel:
+        return cents % nickel
+    else:
+        return 0
+
+def calculate_pennies(cents):
+    penny = 1
+    if cents >= penny:
+        return cents % penny
+    else:
+        return 0
+
+main()
+
