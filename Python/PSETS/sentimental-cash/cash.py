e87fa1cddc93e183ab58df8e2d80868970da1738
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-08-19 08:32:50 +0000

    Fri, Aug 19, 2022, 8:32 AM +00:00

--- Python/PSETS/sentimental-cash/cash.py
+++ Python/PSETS/sentimental-cash/cash.py
@@ -4,27 +4,39 @@ from cs50 import get_float
 
 
 def main():
+    # Prompt the user for owed change
     change = get_float("Change owed: ")
+
+    # Prompt the user forever untill valid input
     while change < 0:
         change = get_float("Change owed: ")
 
+    # Convert change into number of cents
     cents = change*100
 
+    # Calculate number of quarters
     quarters = calculate_quarters(cents)
     cents -= quarters*25
 
+    # Calculate number of dimes
     dimes = calculate_dimes(cents)
     cents -= dimes*10
 
+    # Calculate number of nickels
     nickels = calculate_nickels(cents)
     cents -= nickels*5
 
+    # Calculate number of pennies
     pennies = calculate_pennies(cents)
     cents -= pennies*1
 
+    # Sum up all the coins
     total_coins = quarters + dimes + nickels + pennies
+
+    # Print the least number of coins required
     print(round(total_coins))
 
+# Calculates the number of quarters by taking cents as input
 def calculate_quarters(cents):
     quarter = 25
     if cents >= quarter:
@@ -34,6 +46,7 @@ def calculate_quarters(cents):
     else:
         return 0
 
+# Calculates the number of dimes by taking cents as input
 def calculate_dimes(cents):
     dime = 10
     if cents >= dime:
@@ -43,6 +56,7 @@ def calculate_dimes(cents):
     else:
         return 0
 
+# Calculates the number of pennies by taking cents as input
 def calculate_nickels(cents):
     nickel = 5
     if cents >= nickel:
@@ -52,6 +66,7 @@ def calculate_nickels(cents):
     else:
         return 0
 
+# Calculates the number of pennies by taking cents as input
 def calculate_pennies(cents):
     penny = 1
     if cents >= penny:
