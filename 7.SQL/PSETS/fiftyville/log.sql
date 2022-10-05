a9392567ecf15d114ecd11b5c8fb91227174a551
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-10-05 11:48:24 +0000

    Wed, Oct 5, 2022, 11:48 AM +00:00

--- 7.SQL/PSETS/fiftyville/log.sql
+++ 7.SQL/PSETS/fiftyville/log.sql
@@ -1,4 +1,4 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
 -- SELECT * FROM crime_scene_reports WHERE year = '2021' AND month = '7' AND day = '28' AND street = 'Humphrey Street';
-.schema
+SELECT * FROM crime_scene_reports;
