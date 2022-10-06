6c7a244818c2b89d7d1ad11fef41491e403eab77
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-10-06 14:52:04 +0000

    Thu, Oct 6, 2022, 2:52 PM +00:00

--- 7.SQL/PSETS/fiftyville/tmp.sql
+++ 7.SQL/PSETS/fiftyville/tmp.sql
@@ -5,18 +5,6 @@ SELECT name
     IN (SELECT name
             FROM interviews
             WHERE year = '2021' AND month = '7' AND day = '28')
-    AND (phone_number
-        IN (SELECT caller
-                FROM phone_calls
-                WHERE year = '2021' AND month = '7' AND day = '28' AND duration < 60))
-        OR phone_number IN (SELECT receiver
-                    FROM phone_calls
-                    WHERE year = '2021' AND month = '7' AND day = '28' AND duration < 60)
-        OR license_plate
-        IN (SELECT license_plate
-                FROM bakery_security_logs WHERE year = '2021' AND month = '7' AND day = '28' AND minute < 10)
-    AND passport_number
-    IN (SELECT passport_number
-            FROM passengers
-            WHERE flight_id
-            IN (SELECT id FROM flights WHERE year = '2021' AND month = '7' AND day = '28'))
+
+
+SELECT name FROM people
