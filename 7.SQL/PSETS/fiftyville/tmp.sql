1c76d08b7da4aa1ec2ae61804b664ced9b1afcfb
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-10-06 09:50:20 +0000

    Thu, Oct 6, 2022, 9:50 AM +00:00

--- 7.SQL/PSETS/fiftyville/tmp.sql
+++ 7.SQL/PSETS/fiftyville/tmp.sql
@@ -1,2 +1,2 @@
 
-SELECT * FROM atm_transactions WHERE year = '2021' AND month = '7' AND day = '28'
+SELECT * FROM atm_transactions WHERE year = '2021' AND month = '7' AND day = '28' AND atm_location = 'Leggett Street' AND transaction_type = 'withdraw';
