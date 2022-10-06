a92a38e879d17574c300156d075abd7145ea6ff6
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-10-06 17:12:30 +0000

    Thu, Oct 6, 2022, 5:12 PM +00:00

--- 7.SQL/PSETS/fiftyville/log.sql
+++ 7.SQL/PSETS/fiftyville/log.sql
@@ -27,6 +27,15 @@ SELECT people.name,atm_transactions.amount
                 AND atm_transactions.day = '28'
                 AND atm_transactions.atm_location = 'Leggett Street'
                 AND atm_transactions.transaction_type = 'withdraw';
+
+
+SELECT people.name, phone_calls.duration
+        FROM people
+        JOIN phone_calls
+        ON people.phone_number = phone_calls.caller
+        WHERE year = '2021' AND month = '7' AND day = '28' AND phone_calls.duration <= 60
+        ORDER BY phone_calls.duration;
+
 -- SELECT name
 --     FROM people
 --     WHERE name
