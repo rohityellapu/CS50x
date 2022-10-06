63fb95ab86da2cd6faa18c5fc4e7525d1560bfed
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-10-06 09:04:41 +0000

    Thu, Oct 6, 2022, 9:04 AM +00:00

--- 7.SQL/PSETS/fiftyville/log.sql
+++ 7.SQL/PSETS/fiftyville/log.sql
@@ -2,4 +2,4 @@
 
 -- SELECT * FROM crime_scene_reports WHERE year = '2021' AND month = '7' AND day = '28' AND street = 'Humphrey Street';
 -- .schema
-SELECT * FROM interviews WHERE year = '2021' AND month = '7' AND day = '28' AND street = 'Humphrey Street'
+SELECT * FROM interviews WHERE year = '2021' AND month = '7' AND day = '28';
