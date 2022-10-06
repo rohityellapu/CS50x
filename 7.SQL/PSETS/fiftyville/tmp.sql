2dd0e300b48cb3d0d30160099a058b7aeee43994
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-10-06 17:27:19 +0000

    Thu, Oct 6, 2022, 5:27 PM +00:00

--- 7.SQL/PSETS/fiftyville/tmp.sql
+++ 7.SQL/PSETS/fiftyville/tmp.sql
@@ -1,4 +1,4 @@
 SELECT people.name AS accomplice
         FROM people
         JOIN phone_calls ON people.phone_number = phone_calls.receiver
-        
+        WHERE phone_calls.caller = (SELECT phone_number FROM people WHERE name = 'Bruce')
