3d4bb446221d5a489c8cda687ea763594c309d09
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-10-06 15:37:47 +0000

    Thu, Oct 6, 2022, 3:37 PM +00:00

--- 7.SQL/PSETS/fiftyville/log.sql
+++ 7.SQL/PSETS/fiftyville/log.sql
@@ -9,6 +9,16 @@ SELECT name,transcript
             FROM interviews
             WHERE year = '2021' AND month = '7' AND day = '28' AND transcript LIKE '%bakery%';
 
+
+
+SELECT people.name, bakery_security_logs.activity
+        FROM people
+        JOIN bakery_security_logs
+        ON people.license_plate = bakery_security_logs.license_plate
+        WHERE year = '2021' AND month = '7' AND day = '28' AND minute <= 10 AND activity = 'exit';
+
+
+
 -- SELECT name
 --     FROM people
 --     WHERE name
