8096837d00a96e0df6b36a7ef0da0264389189ae
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-10-06 14:54:22 +0000

    Thu, Oct 6, 2022, 2:54 PM +00:00

--- 7.SQL/PSETS/fiftyville/tmp.sql
+++ 7.SQL/PSETS/fiftyville/tmp.sql
@@ -1,11 +1,10 @@
 
--- SELECT name
---     FROM people
---     WHERE name
---     IN (SELECT name
---             FROM interviews
---             WHERE year = '2021' AND month = '7' AND day = '28')
+SELECT name
+    FROM people
+    WHERE name
+    IN (SELECT name
+            FROM interviews
+            WHERE year = '2021' AND month = '7' AND day = '28')
 
 
--- SELECT name FROM people
-.schema
+SELECT name FROM people
