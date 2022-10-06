a3c93c6f6debf2f768687d4da3e4fe126dad7906
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-10-06 09:56:51 +0000

    Thu, Oct 6, 2022, 9:56 AM +00:00

--- 7.SQL/PSETS/fiftyville/tmp.sql
+++ 7.SQL/PSETS/fiftyville/tmp.sql
@@ -1,2 +1,14 @@
 
-SELECT * FROM atm_transactions WHERE year = '2021' AND month = '7' AND day = '28' AND atm_location = 'Leggett Street' AND transaction_type = 'withdraw';
+SELECT name
+    FROM people
+    WHERE name
+    IN (SELECT name
+            FROM interviews
+            WHERE year = '2021' AND month = '7' AND day = '28')
+    AND (phone_number
+        IN (SELECT caller
+                FROM phone_calls
+                WHERE year = '2021' AND month = '7' AND day = '28' AND duration < 60))
+    OR (phone_number IN (SELECT reciever
+                FROM phone_calls
+                WHERE year = '2021' AND month = '7' AND day = '28' AND duration < 60))
