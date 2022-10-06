64b5ae26846c517ccacf70b7d668380405a4a267
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-10-06 10:03:35 +0000

    Thu, Oct 6, 2022, 10:03 AM +00:00

--- 7.SQL/PSETS/fiftyville/tmp.sql
+++ 7.SQL/PSETS/fiftyville/tmp.sql
@@ -12,6 +12,9 @@ SELECT name
         OR phone_number IN (SELECT receiver
                     FROM phone_calls
                     WHERE year = '2021' AND month = '7' AND day = '28' AND duration < 60)
+        OR license_plate
+        IN (SELECT license_plate
+                FROM bakery_security_logs WHERE year = '2021' AND month = '7' AND day = '28' AND minute < 10)
     AND passport_number
     IN (SELECT passport_number
             FROM passengers
