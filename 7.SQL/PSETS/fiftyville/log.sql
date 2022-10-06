c29f90abf6d4a3dbab040eeecfe5bbc688ade289
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-10-06 09:53:00 +0000

    Thu, Oct 6, 2022, 9:53 AM +00:00

--- 7.SQL/PSETS/fiftyville/log.sql
+++ 7.SQL/PSETS/fiftyville/log.sql
@@ -21,4 +21,9 @@ SELECT name
         IN (SELECT license_plate
                 FROM bakery_security_logs WHERE year = '2021' AND month = '7' AND day = '28' AND minute < 10)
         OR id
-        IN ())
+        IN (SELECT person_id
+                FROM bank_accounts
+                WHERE account_number
+                IN (SELECT account_number
+                        FROM atm_transactions
+                        WHERE year = '2021' AND month = '7' AND day = '28' AND atm_location = 'Leggett Street' AND transaction_type = 'withdraw')))
