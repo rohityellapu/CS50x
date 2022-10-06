25db37c60130dfbf77deafbd2194abf30dfb0ba7
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-10-06 17:29:46 +0000

    Thu, Oct 6, 2022, 5:29 PM +00:00

--- 7.SQL/PSETS/fiftyville/log.sql
+++ 7.SQL/PSETS/fiftyville/log.sql
@@ -11,11 +11,11 @@ SELECT name,transcript
 
 
 
--- SELECT people.name, bakery_security_logs.activity,
---         FROM people
---         JOIN bakery_security_logs
---         ON people.license_plate = bakery_security_logs.license_plate
---         WHERE year = '2021' AND month = '7' AND day = '28' AND minute <= 10 AND activity = 'exit';
+SELECT people.name, bakery_security_logs.activity,
+        FROM people
+        JOIN bakery_security_logs
+        ON people.license_plate = bakery_security_logs.license_plate
+        WHERE year = '2021' AND month = '7' AND day = '28' AND minute <= 25 AND activity = 'exit';
 
 
 SELECT people.name,atm_transactions.amount
@@ -44,31 +44,11 @@ SELECT people.name, airports.city AS destination_city, flights.hour, flights.min
         JOIN airports ON flights.destination_airport_id = airports.id
         WHERE  flights.year = '2021' AND flights.month = '7' AND flights.day = '29'
         ORDER BY flights.hour, flights.minute;
--- SELECT name
---     FROM people
---     WHERE name
---     IN (SELECT name
---             FROM interviews
---             WHERE year = '2021' AND month = '7' AND day = '28')
---     AND (phone_number
---         IN (SELECT caller
---                 FROM phone_calls
---                 WHERE year = '2021' AND month = '7' AND day = '28' AND duration < 60)
---         OR phone_number IN (SELECT receiver
---                     FROM phone_calls
---                     WHERE year = '2021' AND month = '7' AND day = '28' AND duration < 60)
---         OR license_plate
---         IN (SELECT license_plate
---                 FROM bakery_security_logs WHERE year = '2021' AND month = '7' AND day = '28' AND minute < 10)
---         OR id
---         IN (SELECT person_id
---                 FROM bank_accounts
---                 WHERE account_number
---                 IN (SELECT account_number
---                         FROM atm_transactions
---                         WHERE year = '2021' AND month = '7' AND day = '28' AND atm_location = 'Leggett Street' AND transaction_type = 'withdraw')))
---     AND passport_number
---         IN (SELECT passport_number
---                 FROM passengers
---                 WHERE flight_id
---                 IN (SELECT id FROM flights WHERE year = '2021' AND month = '7' AND day = '28'))
+
+
+
+SELECT people.name AS accomplice
+        FROM people
+        JOIN phone_calls ON people.phone_number = phone_calls.receiver
+        WHERE phone_calls.caller = (SELECT phone_number FROM people WHERE name = 'Bruce')
+        AND phone_calls.year = '2021' AND phone_calls.month = '7' AND phone_calls.day = '28' AND phone_calls.duration <= 60
