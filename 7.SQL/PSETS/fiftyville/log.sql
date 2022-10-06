aec4488c4a6520ae0107e4c2d5adebbd5c7da8b1
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-10-06 16:48:28 +0000

    Thu, Oct 6, 2022, 4:48 PM +00:00

--- 7.SQL/PSETS/fiftyville/log.sql
+++ 7.SQL/PSETS/fiftyville/log.sql
@@ -11,11 +11,11 @@ SELECT name,transcript
 
 
 
-SELECT people.name, bakery_security_logs.activity,
-        FROM people
-        JOIN bakery_security_logs
-        ON people.license_plate = bakery_security_logs.license_plate
-        WHERE year = '2021' AND month = '7' AND day = '28' AND minute <= 10 AND activity = 'exit';
+-- SELECT people.name, bakery_security_logs.activity,
+--         FROM people
+--         JOIN bakery_security_logs
+--         ON people.license_plate = bakery_security_logs.license_plate
+--         WHERE year = '2021' AND month = '7' AND day = '28' AND minute <= 10 AND activity = 'exit';
 
 
 SELECT people.name,atm_transactions.amount
--- 7.SQL/PSETS/fiftyville/tmp.sql
+++ 7.SQL/PSETS/fiftyville/tmp.sql
@@ -1,8 +1,3 @@
 
 
 
-SELECT people.name, bakery_security_logs.activity,bakery_security_logs.minute
-        FROM people
-        JOIN bakery_security_logs
-        ON people.license_plate = bakery_security_logs.license_plate
-        WHERE year = '2021' AND month = '7' AND day = '28' AND activity = 'exit' ORDER BY people.name;
