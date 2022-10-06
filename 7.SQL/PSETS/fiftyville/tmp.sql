72d26599b133bdca7ad075f13040fc6d96bbcf6d
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-10-06 15:09:23 +0000

    Thu, Oct 6, 2022, 3:09 PM +00:00

--- 7.SQL/PSETS/fiftyville/tmp.sql
+++ 7.SQL/PSETS/fiftyville/tmp.sql
@@ -1,7 +1,7 @@
 
 SELECT name,transcript
             FROM interviews
-            WHERE year = '2021' AND month = '7' AND day = '28' 
+            WHERE year = '2021' AND month = '7' AND day = '28' AND transcript LIKE '%bakery%';
 
 
 -- SELECT people.name
