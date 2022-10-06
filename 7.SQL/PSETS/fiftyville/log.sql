8f32411c92b8731978bc09c21b97c0b7116d6ed7
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-10-06 15:52:34 +0000

    Thu, Oct 6, 2022, 3:52 PM +00:00

--- 7.SQL/PSETS/fiftyville/log.sql
+++ 7.SQL/PSETS/fiftyville/log.sql
@@ -18,7 +18,15 @@ SELECT people.name, bakery_security_logs.activity
         WHERE year = '2021' AND month = '7' AND day = '28' AND minute <= 10 AND activity = 'exit';
 
 
-
+SELECT people.name,atm_transactions.amount
+        FROM people
+        JOIN bank_accounts ON people.id = bank_accounts.person_id
+        JOIN atm_transactions ON bank_accounts.account_number = atm_transactions.account_number
+        WHERE atm_transactions.year = '2021'
+                AND atm_transactions.month = '7'
+                AND atm_transactions.day = '28'
+                AND atm_transactions.atm_location = 'Leggett Street'
+                AND atm_transactions.transaction_type = 'withdraw';
 -- SELECT name
 --     FROM people
 --     WHERE name
--- 7.SQL/PSETS/fiftyville/tmp.sql
+++ 7.SQL/PSETS/fiftyville/tmp.sql
@@ -1,7 +1,2 @@
 
 
-SELECT people.name,atm_transactions.amount
-FROM people
-INNER JOIN bank_accounts ON people.id = bank_accounts.person_id
-INNER JOIN atm_transactions ON bank_accounts.account_number = atm_transactions.account_number;
--- WHERE atm_transactions.year = '2021' AND atm_transactions.month = '7' AND atm_transactions.day = '28' AND atm_transactions.atm_location = 'Leggett Street' AND atm_transactions.transaction_type = 'withdrawal';
