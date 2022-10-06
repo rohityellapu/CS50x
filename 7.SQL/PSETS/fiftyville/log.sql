af090c6661ce79569eae9f570dfaf7a4ed7b99c0
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-10-06 09:28:43 +0000

    Thu, Oct 6, 2022, 9:28 AM +00:00

--- 7.SQL/PSETS/fiftyville/log.sql
+++ 7.SQL/PSETS/fiftyville/log.sql
@@ -8,5 +8,8 @@ SELECT name
     IN (SELECT name
             FROM interviews
             WHERE year = '2021' AND month = '7' AND day = '28')
-    AND phone_number
-    IN (SELECT caller FROM phone_calls WHERE year = '2021' AND month = '7' AND day = '28')
+    AND (phone_number
+    IN (SELECT caller
+            FROM phone_calls
+            WHERE year = '2021' AND month = '7' AND day = '28')
+        OR () )
