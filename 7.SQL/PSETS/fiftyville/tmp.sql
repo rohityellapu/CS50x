1dcf9ba17c24038b8b324efad54ec16dfc5e100c
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-10-06 17:10:01 +0000

    Thu, Oct 6, 2022, 5:10 PM +00:00

--- 7.SQL/PSETS/fiftyville/tmp.sql
+++ 7.SQL/PSETS/fiftyville/tmp.sql
@@ -1,3 +1,8 @@
 
 
-SELECT people.name, z€ 
+SELECT people.name, phone_calls.duration
+        FROM people
+        JOIN phone_calls
+        ON people.phone_number = phone_calls.caller
+        WHERE year = '2021' AND month = '7' AND day = '28' AND phone_calls.duration <= 60
+        ORDER BY phone_calls.duration;
