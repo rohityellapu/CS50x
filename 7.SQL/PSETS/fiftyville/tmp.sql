664a0c8ed19fffefe4657f7cec2f0104470e349e
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-10-06 14:58:18 +0000

    Thu, Oct 6, 2022, 2:58 PM +00:00

--- 7.SQL/PSETS/fiftyville/tmp.sql
+++ 7.SQL/PSETS/fiftyville/tmp.sql
@@ -7,4 +7,4 @@ SELECT name
             WHERE year = '2021' AND month = '7' AND day = '28')
 
 
-SELECT name FROM people WHERE passport_number IN ()
+SELECT people.name FROM people JOIN passengers ON people.passport_number = passengers.passport_number WHERE passport_number IN (SELECT)
