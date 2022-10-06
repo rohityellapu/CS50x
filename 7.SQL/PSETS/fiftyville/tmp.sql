b00ca357fcc053aa851a7056307b4ba8fa0b3034
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-10-06 15:59:07 +0000

    Thu, Oct 6, 2022, 3:59 PM +00:00

--- 7.SQL/PSETS/fiftyville/tmp.sql
+++ 7.SQL/PSETS/fiftyville/tmp.sql
@@ -5,4 +5,4 @@ SELECT people.name, bakery_security_logs.activity,bakery_security_logs.minute
         FROM people
         JOIN bakery_security_logs
         ON people.license_plate = bakery_security_logs.license_plate
-        WHERE year = '2021' AND month = '7' AND day = '28' AND activity = 'exit';
+        WHERE year = '2021' AND month = '7' AND day = '28' AND activity = 'exit' ORDER BY people.name;
