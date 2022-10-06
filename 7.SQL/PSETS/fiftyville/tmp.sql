fd5757bd0d5ebc473cb5d9d247e867c8a76c8bd6
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-10-06 15:08:16 +0000

    Thu, Oct 6, 2022, 3:08 PM +00:00

--- 7.SQL/PSETS/fiftyville/tmp.sql
+++ 7.SQL/PSETS/fiftyville/tmp.sql
@@ -1,14 +1,11 @@
 
 SELECT name,transcript
-    FROM people
-    WHERE name
-    IN (SELECT name
             FROM interviews
-            WHERE year = '2021' AND month = '7' AND day = '28');
+            WHERE year = '2021' AND month = '7' AND day = '28' 
 
 
-SELECT people.name
-        FROM people
-        JOIN passengers ON people.passport_number = passengers.passport_number
-        WHERE passengers.flight_id
-        IN (SELECT id FROM flights WHERE year = '2021' AND month = '7' AND day = '28');
+-- SELECT people.name
+--         FROM people
+--         JOIN passengers ON people.passport_number = passengers.passport_number
+--         WHERE passengers.flight_id
+--         IN (SELECT id FROM flights WHERE year = '2021' AND month = '7' AND day = '28');
