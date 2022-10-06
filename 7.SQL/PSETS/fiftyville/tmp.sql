fb0bf960f7296c1ef27319a08b8c0c5d0bb9febf
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-10-06 15:43:09 +0000

    Thu, Oct 6, 2022, 3:43 PM +00:00

--- 7.SQL/PSETS/fiftyville/tmp.sql
+++ 7.SQL/PSETS/fiftyville/tmp.sql
@@ -1,1 +1,7 @@
 
+
+SELECT people.name,atm_transactions.amount
+FROM people
+JOIN bank_accounts ON people.id = bank_accounts.person_id
+JOIN atm_transactions ON bank_accounts.account_number = atm_transactions.account_number
+WHERE year = '2021' AND month = '7' AND day = '28' AND atm_location = 'Leggett Street' AND transaction_type = 'withdrawal';
