c1c156cbb20c843952732e650e81bfbe25a3efcb
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-10-06 15:49:24 +0000

    Thu, Oct 6, 2022, 3:49 PM +00:00

--- 7.SQL/PSETS/fiftyville/tmp.sql
+++ 7.SQL/PSETS/fiftyville/tmp.sql
@@ -3,5 +3,5 @@
 SELECT people.name,atm_transactions.amount
 FROM people
 INNER JOIN bank_accounts ON people.id = bank_accounts.person_id
-INNER JOIN atm_transactions ON bank_accounts.account_number = atm_transactions.account_number
-WHERE atm_transactions.year = '2021' AND atm_transactions.month = '7' AND atm_transactions.day = '28' AND atm_transactions.atm_location = 'Leggett Street' AND atm_transactions.transaction_type = 'withdrawal';
+INNER JOIN atm_transactions ON bank_accounts.account_number = atm_transactions.account_number;
+-- WHERE atm_transactions.year = '2021' AND atm_transactions.month = '7' AND atm_transactions.day = '28' AND atm_transactions.atm_location = 'Leggett Street' AND atm_transactions.transaction_type = 'withdrawal';
