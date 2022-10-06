aa41cd1c29b7745d7f9b2d6410bf386c937de0fd
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-10-06 17:19:21 +0000

    Thu, Oct 6, 2022, 5:19 PM +00:00

--- 7.SQL/PSETS/fiftyville/tmp.sql
+++ 7.SQL/PSETS/fiftyville/tmp.sql
@@ -1,2 +1,7 @@
 
-SELECT people.name, flights.
+SELECT people.name, airports.city
+        FROM people
+        JOIN passengers ON people.passport_number = passengers.passport_number
+        JOIN flights ON passengers.flight_id = flights.id
+        JOIN airports ON flights.destination_airport_id = airports.id
+        WHERE  year = '2021' AND month = '7' AND day = '29'
