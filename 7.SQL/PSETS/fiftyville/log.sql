76740eccd4f0b5e197e02e13332b0e0cc449c3be
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-10-07 09:49:59 +0000

    Fri, Oct 7, 2022, 9:49 AM +00:00

--- 7.SQL/PSETS/fiftyville/log.sql
+++ 7.SQL/PSETS/fiftyville/log.sql
@@ -2,8 +2,8 @@
 
 -- First we go through the crime scenes happened in July 28th 2021 at Humphrey Street;
 SELECT *
-FROM crime_scene_reports
-WHERE year = '2021' AND month = '7' AND day = '28' AND street = 'Humphrey Street';
+        FROM crime_scene_reports
+        WHERE year = '2021' AND month = '7' AND day = '28' AND street = 'Humphrey Street';
 
 -- Two crime incidents happened on that day, one is related to Theft and other is related to Littering .We can exlude the latter case
 -- As per description in crime report, the theft took place at 10:15 AM at the Humphrey Street bakety.
@@ -51,19 +51,23 @@ SELECT people.name, phone_calls.duration
         WHERE year = '2021' AND month = '7' AND day = '28' AND phone_calls.duration <= 60
         ORDER BY people.name;
 
--- Then we list names of people who booked the earliest flights on 29th July from Fiftyville with
+-- Then we list names of people who booked the earliest flights on 29th July from Fiftyville along with destination city of the flights and them to suspect list
 SELECT people.name, airports.city AS destination_city, flights.hour, flights.minute
         FROM people
         JOIN passengers ON people.passport_number = passengers.passport_number
         JOIN flights ON passengers.flight_id = flights.id
         JOIN airports ON flights.destination_airport_id = airports.id
-        WHERE  flights.year = '2021' AND flights.month = '7' AND flights.day = '29' AND flights.hour <= 12 AND flights.origin_airport_id = (SELECT id FROM airports WHERE city = 'Fiftyville')
+        WHERE  flights.year = '2021' AND flights.month = '7' AND flights.day = '29' AND flights.hour < 12 AND flights.origin_airport_id = (SELECT id FROM airports WHERE city = 'Fiftyville')
         ORDER BY flights.hour, flights.minute,people.name;
 
-
-
+-- We found the earliest flights starting at 8:20 AM, 9: 30 AM , flying to New York City and Chicago
+-- Among all the suspect lists, Bruce is common in all. Therefore, Bruce must be thief and is flying to New York City.
+-- To find the accomplice we list the name of person who recieved the call from Bruce at the day of theft
 SELECT people.name AS accomplice
         FROM people
         JOIN phone_calls ON people.phone_number = phone_calls.receiver
         WHERE phone_calls.caller = (SELECT phone_number FROM people WHERE name = 'Bruce')
         AND phone_calls.year = '2021' AND phone_calls.month = '7' AND phone_calls.day = '28' AND phone_calls.duration <= 60
+
+
+-- There is only one person named Robin, therefore he is the accomplice who helped Bruce in booking the flight ticket.
