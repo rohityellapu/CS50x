655a98c8a88940e8ee4473902710f741755f9326
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-10-07 09:21:58 +0000

    Fri, Oct 7, 2022, 9:21 AM +00:00

--- 7.SQL/PSETS/fiftyville/log.sql
+++ 7.SQL/PSETS/fiftyville/log.sql
@@ -1,10 +1,11 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+-- First we go through the crime scenes happened in July 28th 2021 at Humphrey Street;
 SELECT *
 FROM crime_scene_reports
 WHERE year = '2021' AND month = '7' AND day = '28' AND street = 'Humphrey Street';
 
-
+-- 
 SELECT name,transcript
             FROM interviews
             WHERE year = '2021' AND month = '7' AND day = '28' AND transcript LIKE '%bakery%';
