bd15a2b7554c35eda125b440194f363362ea9cbf
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-10-28 11:44:33 +0000

    Fri, Oct 28, 2022, 11:44 AM +00:00

--- 9.Flask/Lab/birthdays/app.py
+++ 9.Flask/Lab/birthdays/app.py
@@ -23,7 +23,7 @@ def after_request(response):
 
 
 @app.route("/", methods=["GET", "POST"])
-def index(ind):
+def index():
     if request.method == "POST":
 
         # TODO: Add the user's entry into the database
@@ -34,6 +34,6 @@ def index(ind):
 
         # TODO: Display the entries in the database on index.html
         ind = db.execute(f"SELECT * FROM birthdays")
-        return render_template("index.html")
+        return render_template("index.html", ind=ind)
 
 
