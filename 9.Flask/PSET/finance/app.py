0752542943150a8c7058f1a9efd7072aab40b399
rohityellapu <100713605+rohityellapu@users.noreply.github.com>
2022-12-17 13:31:28 +0000

    Sat, Dec 17, 2022, 1:31 PM +00:00

--- 9.Flask/PSET/finance/app.py
+++ 9.Flask/PSET/finance/app.py
@@ -54,7 +54,7 @@ def index():
         total_stocks_value += stock['total']
     user_cash = db.execute('SELECT cash FROM users WHERE username = ?', session['user'])[0]['cash']
 
-    return render_template('index.html', stocks=user_stocks, cash=round(user_cash,2), total=total_stocks_value, usd=usd)
+    return render_template('index.html', stocks=user_stocks, cash=usd(user_cash), total=total_stocks_value, usd=usd)
 
 
 
